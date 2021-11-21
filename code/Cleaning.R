43a6cb56bc98c427879eaa0d0bbadfcb29f95d95
Jason Rosenthal <jason@eduroamstud-130-82-254-251.unisg.ch>
2021-11-21 12:05:15 +0100

    some cleaning plus new legend data

--- code/Cleaning.R
+++ code/Cleaning.R
@@ -17,7 +17,7 @@ load("data/rent_listings_raw.RData")
 lapply(D, function(x) table(is.na(x)))
 
 # preliminary selection of important variables
-keep_for_now <- c("rent_full", "area", "home_type", "GDENAMK", "KTKZ", "zipcode", "GKODE", "GKODN", "PLZNAME",
+keep_for_now <- c("rent_full", "area", "GDENAMK", "KTKZ", "zipcode", "GKODE", "GKODN", "PLZNAME",
                   "descr", "floors", "furnished", "lat", "lon", "date","month", "quarter_general", # furnished kept for tokenization
                   "msregion", "rooms",  "year_built", "newly_built", "balcony", # check if newly_built is year_built == 2019
                   "Micro_rating_new", "Micro_rating_NoiseAndEmission_new", "Micro_rating_Accessibility_new",
@@ -57,9 +57,9 @@ D_selection <- D[,keep_for_now]
 
 
 
-D_selection <- replace(D_selection,"TRUE",1)
+# D_selection <- replace(D_selection,"TRUE",1)
 #taking all variables with 1 or TRUE, converting it into 1s and 0s
-temp <- apply(D_selection, 2, unique)
+
 
 
 bin <- c("balcony", "furnished")
@@ -68,11 +68,50 @@ bin <- c("balcony", "furnished")
 D_selection[,bin] = apply(D_selection[,bin], 2, function(x) replace_na(x,0))
 
 
+##### Sunday's work
+D_omit <- D_selection[!is.na(D_selection$area),] # area is crucial for analysis
+
+View(D %>%
+       filter(!is.na(area)) %>%
+       mutate("rent_m2" = rent_full/area) %>%
+       select("rent_full", "area", "rent_m2", "rent_m2_pix_avg_km2", "GDENAMK", "KTKZ", "zipcode", "GKODE", "GKODN", "PLZNAME",
+              "descr", "floors", "furnished", "lat", "lon", "date","month", "quarter_general",
+              "msregion", "rooms",  "year_built", "newly_built", "balcony",
+              "Micro_rating_new", "Micro_rating_NoiseAndEmission_new", "Micro_rating_Accessibility_new",
+              "Micro_rating_DistrictAndArea_new", "Micro_rating_SunAndView_new",
+              "Micro_rating_ServicesAndNature_new",
+              "dist_to_haltst", "dist_to_highway", "dist_to_school_1",
+              "dist_to_train_stat", "apoth_pix_count_km2", "restaur_pix_count_km2", "superm_pix_count_km2") %>%
+       arrange(rent_full, rent_m2))
+       
+
+## FILTERING METHOD ------
+# we are going to calculate mean m2 rents for all "arbeitsmarktregionen" and then
+# we filter outliers (as measured by something like a boxplot or some standard
+# deviations from the mean). We then consider only values within the bounds
+
+# computing arbeitsmarktregionen
+# merging ms regions with arbeitsmarktregionen
+ms_avg <- D %>% # we first compute mean m2 per arbeitsmarkt regionen
+  filter(!is.na(area)) %>%
+  mutate("rent_m2" = rent_full/area) %>%
+  select("rent_full", "area", "rent_m2", "rent_m2_pix_avg_km2", "GDENAMK", "KTKZ", "zipcode", "GKODE", "GKODN", "PLZNAME",
+         "descr", "floors", "furnished", "lat", "lon", "date","month", "quarter_general",
+         "msregion", "rooms",  "year_built", "newly_built", "balcony",
+         "Micro_rating_new", "Micro_rating_NoiseAndEmission_new", "Micro_rating_Accessibility_new",
+         "Micro_rating_DistrictAndArea_new", "Micro_rating_SunAndView_new",
+         "Micro_rating_ServicesAndNature_new",
+         "dist_to_haltst", "dist_to_highway", "dist_to_school_1",
+         "dist_to_train_stat", "apoth_pix_count_km2", "restaur_pix_count_km2", "superm_pix_count_km2") %>%
+  group_by(msregion) %>%
+  summarise(rent_mean_ms = mean(rent_full))
+
+
 D_clean <- D_selection %>%
   select(-c(year_built,descr)) %>%
   na.omit(D_selection)
 
-apply(D_selection, 2, function(x) sum(is.na(x)))
+
 
 
 
