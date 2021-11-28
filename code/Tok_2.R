7642c6f6b234043acec6835cfe70eea51c9fa311
Amelie-Mad <94747136+Amelie-Mad@users.noreply.github.com>
2021-11-28 09:25:14 +0100

    Create Tok_2.R

---
+++ code/Tok_2.R
@@ -0,0 +1,139 @@
+################################################################################
+#                                                                              #
+# DSF Group Project                                                            #
+# Script 3: Tokenisation                                                       #
+# 20.11.2021                                                                   #
+#                                                                              #        
+################################################################################
+
+rm(list = ls())
+
+load("data/rent_listings_raw.RData")
+
+library(tidytext)
+library(tidyverse)
+library(lubridate)
+library(textdata)
+library(tidyr)
+
+# we can only do tokenisation for observations with a "descr" or appartment description 
+
+D_tok = data_analyzed %>% 
+  filter(is.na(descr)==F) %>% 
+  select(rowid,descr,furnished,rooms,balcony, home_type)
+
+
+tok <- D_tok %>% unnest_tokens(word, descr, token = "words") 
+
+improved_data_analyzed = data_analyzed
+
+### furnished ----
+
+furnished_words <- c("furnished", "furniture",
+                     "meublé", "meubles", "meuble", "meublées", "meublée", "mobilier",
+                     "möbliert", "möblierte", "möbel", "möbeln",
+                     "ammobiliato", "mobilio", "mobili") 
+
+furnished_appartments <- tok %>% filter(furnished == 0) %>%
+  filter(word %in% furnished_words) 
+
+improved_data_analyzed[unique(furnished_appartments$rowid),"furnished"] <- 1 # 1723 obs
+
+# problem: doesn't take into account "non-meublé" - trying to solve this with bigrams
+
+negation <- c("non", "no", "nicht", "ohne", "pas")
+
+bigrams <- D_tok %>% 
+  unnest_tokens(bigram, descr, token = "ngrams", n = 2, drop = F)
+
+no_furniture <- bigrams %>%
+  filter(rowid %in% furnished_appartments$rowid) %>% #if remove this line get rid of the 2
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  filter(word1 %in% negation) %>%
+  filter(word2 %in% furnished_words) %>%
+  unite(bigram, word1, word2, sep = " ", remove = F) %>%
+  distinct(rowid, .keep_all = TRUE) # 110 obs 
+
+# by doing this, also found 2 which were categorised as furnished but arent (we don't care)
+
+improved_data_analyzed[unique(no_furniture$rowid),"furnished"] <- 0 # 108 obs
+
+# still a problem with those giving a choice I suggest we just accept to miss out on those  
+# maybe more efficient to put the no_furniture part in an ifelse in the previous step?
+
+### balcony ----
+
+balcony_words <- c("balcon", "Balkon", "balkon", "Balcon", "balcone", "Balcone")
+
+balcony_appartments <- tok %>% filter(balcony == 0) %>%
+  filter(word %in% balcony_words)
+
+improved_data_analyzed[unique(balcony_appartments$rowid),"balcony"] <- 1 # 8997 obs
+
+### home_type ----
+
+homes = c("wohnung","attika","dachwohnung","terrassenwohnung","maisonette",      
+              "studio","loft","ferienwohnung", "attique", "soffitta", "attico","sottotetto")
+
+new_home_types = tok %>% filter(word %in% homes)
+
+new_home_types$home_type =tolower(new_home_types$home_type)
+
+new_home_types = new_home_types %>% 
+  filter(home_type != word) %>% # 7706 obs, but do we really want to overwrite?
+  filter(word != "wohnung") %>% # 1967 obs, removing since its the "default" word
+  filter(home_type == "wohnung") %>% # 1596 obs, #deciding not to overwrite the "special" hometypes
+  distinct(rowid, .keep_all = TRUE) # 1417 obs (all of those where before i included more languages)
+
+### number of rooms ----
+
+number <- as.character(c(seq(from=1, to=10, by=0.5),
+                         paste(seq(from=1, to = 10, by=1), sep= ",",5))) 
+
+room <- as.character(c("zimmer", "Zimmer", "pièces", "pièce", "pcs", "stanze"))
+
+new_rooms <- bigrams %>%
+  filter(is.na(rooms) == T) %>%
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  filter(word1 %in% number) %>%
+  filter(word2 %in% room) %>%
+  unite(bigram, word1, word2, sep = " ", remove = F) %>%
+  distinct(rowid, .keep_all = TRUE)  # those where it appears more than once... 
+                                     # hoping that the 1st one is the correct one
+
+improved_data_analyzed[unique(new_rooms$rowid),"rooms"] <- as.numeric( 
+  ifelse(new_rooms$word1 %in% number[1:19], 
+         new_rooms$word1, 
+         (paste((substr(new_rooms$word1,1,1)), sep= ".",5))))
+
+              # simplistic but it works: catches 472 more observations
+
+# dealing with the 1/2 (appearing as 1 2), overwriting what was wrongly caught by the bigrams
+
+trigrams <- D_tok %>% 
+  filter(is.na(rooms) == T) %>%
+  unnest_tokens(trigram, descr, token = "ngrams", n = 3, drop = F) %>% 
+  separate(trigram, c("word1", "word2", "word3"), sep = " ") %>%
+  filter((word2 == "1" & word3 =="2")) %>%
+  unite(word2, word2, word3, sep = "/", remove = F) %>%
+  filter(word1 %in% number) %>%
+  distinct(rowid, .keep_all = TRUE)
+
+improved_data_analyzed[unique(trigrams$rowid),"rooms"] <- as.numeric( 
+  paste(trigrams$word1, sep = ".",5))
+
+# trying stuff out with area on D ----
+D <- rowid_to_column(D)
+
+area_bigrams <- D %>% filter(is.na(area) == T) %>%
+  unnest_tokens(bigram, descr, token = "ngrams", n = 2, drop = F) %>%
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  filter(word1 %in% as.character(seq(from= 25, to = 200, by = 1))) %>%
+  filter(word2 == c("m2", "m")) %>%
+  unite(bigram, word1, word2, sep = " ", remove = F) %>%
+  distinct(rowid, .keep_all = TRUE) #334 obs
+
+# D[unique(area_bigrams$rowid),"area"] <- as.numeric(area_bigrams$word1) 
+
+
+
