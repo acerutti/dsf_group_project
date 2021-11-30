84b45a0a82d5244771a13963213113ef171e832b
MB00R <72549436+MB00R@users.noreply.github.com>
2021-11-30 08:42:17 +0100

    Final Script Setup

---
+++ code/Rent_predictions_Bisges_Cerutti_Madrona_Rosenthal.R
@@ -0,0 +1,27 @@
+################################################################################
+#
+# Data Science Fundamentals Group Project
+# Fall 2021
+# Final script 
+# 05.12.2021
+# Maximilian Georg Hans Bisges
+# Alessandra Elivia Eugenia Cerutti
+# Amelie Carla Hoa Madrona
+# Jason Rosenthal
+################################################################################
+
+### Data Import-----
+
+
+### Data Cleaning----
+
+
+### Tokenisation----
+
+
+
+
+### Data Analysis----
+
+
+# Modelling----
